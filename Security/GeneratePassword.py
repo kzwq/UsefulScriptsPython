00c1839d0139134cab2d2919ed9606d8464a732b
kzwq <krasnove011@gmail.com>
2023-01-29 12:01:41 +0300

    Some improvements

--- Security/GeneratePassword.py
+++ Security/GeneratePassword.py
@@ -1,12 +1,13 @@
 import string
-import random
-import sys
+from random import sample
+from sys import exit
+print('Generator')
+print()
 
 chars = ""
 
 print("Let's select chars for your future password")
 if input(f"Do you want include big letters? ({string.ascii_uppercase}) [Y] ").strip() == "":
-    print('ye')
     chars += string.ascii_uppercase
 
 if input(f"Do you want include small letters? ({string.ascii_lowercase}) [Y] ").strip() == "":
@@ -17,15 +18,15 @@ if input(f"Do you want include numbers? ({string.digits}) [Y] ").strip() == "":
 
 if chars.replace(' ', '') == '':
     print('There are no available letters to generate password!')
-    sys.exit(127)
+    exit(127)
 
 
 len_str = input("How many letters do I need to generate? ")
 length = 0
 try: length = int(len_str)
-except ValueError: print("It's not a number!"); sys.exit(127)
+except ValueError: print("It's not a number!"); exit(127)
 
-genereated = ''.join(random.sample(chars, length))
+genereated = ''.join(sample(chars, length))
 print()
 print(genereated)
 print()
